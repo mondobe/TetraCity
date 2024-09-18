e06b2356ca1d308e6099bb27fcd4d3defdb44a40
Mondobe <spiesfortheprize@gmail.com>
2024-09-18 14:58:48 -0400

    Add UI dialogue box
    Thanks to Mike for the art

---
+++ scripts/ui/gameplay/dialogue_box.gd
@@ -0,0 +1,30 @@
+class_name DialogueBox
+extends Control
+
+@export var padding: float
+@onready var rect: NinePatchRect = $NinePatchRect
+@onready var label: RichTextLabel = $NinePatchRect/RichTextLabel
+
+func _ready() -> void:
+	set_border_width(400)
+	set_text("Good morning, Madam Mayor! It looks like City Hall has just been delivered.")
+
+func _padding() -> Vector2:
+	return Vector2(padding, padding)
+
+func resize_content(size: Vector2) -> void:
+	label.position = Vector2(13, 13) + _padding()
+	rect.size = size + Vector2(26, 26) + 2 * _padding()
+	label.size = size
+
+func resize_borders(size: Vector2) -> void:
+	resize_content(size - Vector2(26, 26))
+
+func set_border_width(width: float) -> void:
+	resize_borders(Vector2(width, rect.size.y))
+
+func set_text(text: String) -> void:
+	label.text = text
+	var text_width = label.get_content_width()
+	var text_height = label.get_content_height()
+	resize_content(Vector2(text_width, text_height))
