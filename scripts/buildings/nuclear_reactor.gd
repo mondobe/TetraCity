2a799a1e305fcae5ba346916674adff61c0fa311
Peeda <107806301+Peeda@users.noreply.github.com>
2024-09-20 18:50:55 -0400

    made the nuclear reactor work.

--- scripts/buildings/nuclear_reactor.gd
+++ scripts/buildings/nuclear_reactor.gd
@@ -3,6 +3,6 @@ extends Node
 ## Multiply adjacent coin bonuses by five, by adding four times their CPD
 func get_bonus() -> BuildingBonus:
 	var coins = get_parent().get_adjacent_buildings().reduce(
-		func(accum, building) : return 4 * building.bonus.coins
+		func(accum, building) : return 4 * building.bonus.get_bonus().coins, 0
 	)
 	return BuildingBonus.new().with_coins(coins)
