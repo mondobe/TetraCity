c258bfbd904d89f6b097236e55266cfe22f17219
Nathan Xie <nathankeystone@hotmail.com>
2024-09-25 13:03:21 -0400

    update school.gd
    optimized the code to not rely on counting each individual building and instead simple add coins based on the size of the array

--- scripts/buildings/school.gd
+++ scripts/buildings/school.gd
@@ -7,7 +7,5 @@ func get_bonus() -> BuildingBonus:
 	var bonus: int = 0
 	var building_grid = get_parent().building_grid;
 	if(building_grid != null):
-		print("Full")
-		for building: Building in building_grid.buildings:
-			bonus += 1
+		bonus += building_grid.buildings.size();
 	return BuildingBonus.new().with_coins(bonus)
