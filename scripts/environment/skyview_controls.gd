866d54bc4587d7e7cdbdf56d55fc8dab8b60b6eb
Mondobe <spiesfortheprize@gmail.com>
2024-10-03 21:25:44 -0400

    Very quick fix
    Coin counter goes down when buying a building...
    How did I forget this

--- scripts/environment/skyview_controls.gd
+++ scripts/environment/skyview_controls.gd
@@ -78,6 +78,7 @@ func buy_button(box: NpcDialogueBox) -> void:
 	if _world_stats.coins < box.price:
 		ignore_button(box)
 		return
+	_world_stats.coins -= box.price
 	_moving_camera.lock_to_camera_mode(MovingCamera.CameraMode.GROUND)
 	_building_grid.make_and_place(box.variation)
 	box.balloon.queue_free()
