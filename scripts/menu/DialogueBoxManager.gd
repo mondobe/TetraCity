53c12c1d2780d08bdee79147a2871c0ecec4bb5e
Nathan Xie <nathankeystone@hotmail.com>
2024-10-16 13:52:59 -0400

    Scaled textboxes and images
    Made it so that the cutscene textboxes are properly scales. Created a TextureRect which serves as the image frame. It accesses its images through a Texture2D array which is cycled as the player goes through the cutscene. Takes input from any button press.

--- scripts/menu/DialogueBoxManager.gd
+++ scripts/menu/DialogueBoxManager.gd
@@ -6,23 +6,34 @@ extends Node
 
 @export var conversation: Array[String]
 
+@export var image: Array[Texture2D]
+
 @onready var currentString: int = 0
+
+@export var imageFrame: TextureRect
 
 # Called when the node enters the scene tree for the first time.
 # 1920 x 1080
 # 934 x 100
 func _ready():
-	var test = _dialogue_box_spawner.spawn_cutscene_box_at_screen(Vector2(0, 439))
+	var test = _dialogue_box_spawner.spawn_cutscene_box_at_screen()
+	test.update_size()
 	test.set_speaker_text(conversation[currentString]);
+	imageFrame.texture = image[currentString]
+	imageFrame.size = DisplayServer.window_get_size(0)
 	test.set_speaker("Godot")
 	textBox = test
 	
 func updateTextbox():
 	textBox.set_speaker_text(conversation[currentString]);
+	imageFrame.texture = image[currentString]
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if(Input.is_action_just_pressed("left_mouse")):
+func _input(event):
+	if(Input.is_anything_pressed()):
 		currentString += 1
 		if(currentString < conversation.size()):
 			updateTextbox()
+		else:
+			pass
+			#Exit the cutscene scene
+
