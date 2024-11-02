f781bb0dc8c0e7a31857bd057397adae92ade5da
Peeda <107806301+Peeda@users.noreply.github.com>
2024-11-02 12:49:34 -0400

    whoops! gotta return that value.

--- scripts/buildings/bank.gd
+++ scripts/buildings/bank.gd
@@ -12,7 +12,7 @@ func get_scale_factor(bd_print: BuildingBlueprint) -> float:
 		func(acc, building): 
 			# I'm assuming equality works here, possibly change later
 			var matches_type = building.blueprint == bd_print
-			acc + 1 if matches_type else acc
+			return acc + 1 if matches_type else acc
 	, 0)
 	return pow(adjust_factor, match_cnt)
 
