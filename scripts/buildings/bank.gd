8df999de8b8927fd65582bd22fc09ff03d8a3631
Mondobe <spiesfortheprize@gmail.com>
2024-11-04 19:50:25 -0500

    Add functionality for bank
    Also, it appears in the world... that part is important

--- scripts/buildings/bank.gd
+++ scripts/buildings/bank.gd
@@ -4,12 +4,12 @@ extends Node
 func get_bonus() -> BuildingBonus:
 	return BuildingBonus.new()
 
-# Count the number of adjacent buildings matching the type of the blueprint parameter 
+# Count the number of adjacent buildings matching the type of the blueprint parameter
 # and calculate the appropriate modifier
 func get_scale_factor(bd_print: BuildingBlueprint) -> float:
 	var adjust_factor: float = 0.75
 	var match_cnt = get_parent().get_adjacent_buildings().reduce(
-		func(acc, building): 
+		func(acc, building):
 			# I'm assuming equality works here, possibly change later
 			var matches_type = building.blueprint == bd_print
 			return acc + 1 if matches_type else acc
@@ -17,4 +17,4 @@ func get_scale_factor(bd_print: BuildingBlueprint) -> float:
 	return pow(adjust_factor, match_cnt)
 
 func get_info_text() -> String:
-	return "For every building adjacent, buildings of that type are 25% cheaper! (stacks)"
+	return "Adjacent buildings are 25% cheaper."
